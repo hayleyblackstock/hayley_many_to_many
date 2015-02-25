350e7d780382b24b404fa216eee20cb13f962c37
Hayley Blackstock <Hayley@Hayleys-MacBook-Air.local>
2015-02-25 15:21:33 -0700

    update view page and review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   def index
     @company_reviews = CompanyReview.all
     @product_reviews = ProductReview.all
+
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
 
+has_many :company_reviews
+has_many :product_reviews
+
 end
