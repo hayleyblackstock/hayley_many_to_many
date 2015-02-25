ebef8851d7cf9157be17483cf7e62f65050054c2
Hayley Blackstock <Hayley@Hayleys-MacBook-Air.local>
2015-02-25 15:28:14 -0700

    add validation to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
  belongs_to :company
  has_many :product_reviews
+
+ validates :company_id, presence: true
+
 end
--- app/models/product_review.rb
+++ app/models/product_review.rb
@@ -3,4 +3,5 @@ class ProductReview < ActiveRecord::Base
 belongs_to :product
 belongs_to :user
 
+validates :title, :description, :product_id, :user_id, :rating, presence: true
 end
